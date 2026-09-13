4eadf50219cebbe89577c4d7a58959d6d812ccdf
Juraj Mesarić <juraj12mesaric9@gmail.com>
2026-09-13 20:01:06 +0200

    dodano spremanje narudzbe u narudzba.json

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import json
 
-with open('zalihe.json') as f:
-    zalihe = json.load(f)
-
+try:
+    with open('zalihe.json') as f:
+        zalihe = json.load(f)
+except FileNotFoundError:
+    print("Ta datoteka ne postoji")
+except json.JSONDecodeError:
+    print("JSON datoteka nije ispravno napisana")
 
 def trebam_nar(proizvod):
     return (proizvod['minimum'] > proizvod['kolicina'])
@@ -12,8 +16,19 @@ def kol_nar(proizvod):
 
 
 def izvjestaj(zalihe):
+    report = {}
     for i in zalihe:
         if trebam_nar(i):
-            print(f"Trebam naruciti {i['naziv']}: {kol_nar(i)} komada")
+            report[i['naziv']] = kol_nar(i)
+    return report
 
 izvjestaj(zalihe)
+
+novi_proizvodi = {"naziv":"Coca Cola 2L", "kolicina":100, "minimum": 35, "maksimum": 100}
+zalihe.append(novi_proizvodi)
+narudzba = izvjestaj(zalihe)
+
+with open("narudzba.json","w") as json_file:
+    data1 = json.dump(narudzba, json_file)
+
+
